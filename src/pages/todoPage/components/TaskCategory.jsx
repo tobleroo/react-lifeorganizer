09989c540996fdb9fb2f395cc3e5a0fed569d727
Tobleroo <tobias.jaks@gmail.com>
2023-04-25 21:25:38 +0200

    started on calendar section of todo

--- src/pages/todoPage/components/TaskCategory.jsx
+++ src/pages/todoPage/components/TaskCategory.jsx
@@ -9,11 +9,89 @@ export default function TaskCategory(){
         <div className="taskCategoryBox">
             <div className="one-category">
                 <h2>Task category</h2>
-                <p>task name</p>
+                <div className="oneTaskData">
+                    <div className="taskName">
+                        <h4>Task name</h4>
+                        <div className="optionsBtns">
+                            <b>deleteBtn</b>
+                            <b>addBtn</b>
+                        </div>
+                    </div>
+                    <div className="smallData">
+                        <div className="dataBox">
+                            <b>Frequenzy</b>
+                            <p>Every <b>3</b> days</p>
+                        </div>
+                        <div className="dataBox">
+                            <b>Time To do</b>
+                            <p>45 minutes</p>
+                        </div>
+                        <div className="dataBox">
+                            <b>Times completed</b>
+                            <p>5</p>
+                        </div>
+                    </div>
+                    <div className="descBox">
+                        <b>Description:</b>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae vitae voluptatibus velit officiis excepturi quo itaque debitis suscipit ex optio!</p>
+                    </div>
+                </div>
+
+                <div className="oneTaskData">
+                    <div className="taskName">
+                        <h4>Task name</h4>
+                        <div className="optionsBtns">
+                            <b>deleteBtn</b>
+                            <b>addBtn</b>
+                        </div>
+                    </div>
+                    <div className="smallData">
+                        <div className="dataBox">
+                            <b>Frequenzy</b>
+                            <p>Every <b>3</b> days</p>
+                        </div>
+                        <div className="dataBox">
+                            <b>Time To do</b>
+                            <p>45 minutes</p>
+                        </div>
+                        <div className="dataBox">
+                            <b>Times completed</b>
+                            <p>5</p>
+                        </div>
+                    </div>
+                    <div className="descBox">
+                        <b>Description:</b>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae vitae voluptatibus velit officiis excepturi quo itaque debitis suscipit ex optio!</p>
+                    </div>
+                </div>
+
             </div>
             <div className="one-category">
                 <h2>Task category</h2>
-                <p>task name</p>
+                <div className="oneTaskData">
+                    <div className="taskName">
+                        <h4>Task name</h4>
+                        <b>deleteBtn</b>
+                    </div>
+                    <div className="smallData">
+                        <div className="dataBox">
+                            <b>Frequenzy</b>
+                            <p>Every <b>3</b> days</p>
+                        </div>
+                        <div className="dataBox">
+                            <b>Time To do</b>
+                            <p>45 minutes</p>
+                        </div>
+                        <div className="dataBox">
+                            <b>Times completed</b>
+                            <p>5</p>
+                        </div>
+                    </div>
+                    <div className="descBox">
+                        <b>Description:</b>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae vitae voluptatibus velit officiis excepturi quo itaque debitis suscipit ex optio!</p>
+                    </div>
+                </div>
             </div>
             
 
--- src/pages/todoPage/components/WeeklyCalendarCard.jsx
+++ src/pages/todoPage/components/WeeklyCalendarCard.jsx
@@ -6,16 +6,46 @@ export default function WeeklyCalendarCard(){
 
 
     return (
-        <div className="CalendarCard">
+        <div className="MonthlyCard">
+            <h3>februari</h3>
+            <div className="WeekBox">
+                <h3>week 3</h3>
+                <div className="DayBox">
+                    <p>måndag </p>
+                    <p>02-23-23</p>
+                    <div className="TasksDateBox">
+                        <p>task 1</p>
+                        <p>task 2</p>
+                    </div>
+                </div>
+                <div className="DayBox">
+                    <p>tisdag </p>
+                    <p>02-23-23</p>
+                    <div className="TasksDateBox">
+                        <p>task 1</p>
+                        <p>task 2</p>
+                    </div>
+                </div>
+            </div>
 
-            <h3>week 3</h3>
-            <div className="date-box">
-                <p>måndag </p>
-                <p>02-23-23</p>
-            </div>  
-            <div className="tasks">
-                <h5>task 1</h5>
-                <h5>task 2</h5>
+            <div className="WeekBox">
+                <h3>week 3</h3>
+                <div className="DayBox">
+                    <p>måndag </p>
+                    <p>02-23-23</p>
+                    <div className="TasksDateBox">
+                        <p>task 1</p>
+                        <p>task 2</p>
+                    </div>
+                </div>
+                <div className="DayBox">
+                    <p>tisdag </p>
+                    <p>02-23-23</p>
+                    <div className="TasksDateBox">
+                        <p>task 1</p>
+                        <p>task 2</p>
+                    </div>
+                </div>
             </div>
             
         </div>
